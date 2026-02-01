0bcce748ea503af389cdfc0bf2cf9491cbd95499
Kyle Mathews <mathews.kyle@gmail.com>
2026-02-01 16:15:38 -0700

    cleanup initial route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Container, Flex, Heading, Text, Button } from '@radix-ui/themes'
+import { Container, Flex, Heading, Text } from '@radix-ui/themes'
 
 export const Route = createFileRoute('/')({
   component: HomePage,
@@ -8,12 +8,11 @@ export const Route = createFileRoute('/')({
 function HomePage() {
   return (
     <Container size="2" py="9">
-      <Flex direction="column" gap="4" align="center">
-        <Heading size="9">Welcome</Heading>
+      <Flex direction="column" gap="5" align="center">
+        <Heading size="8">Welcome</Heading>
         <Text size="4" color="gray">
           Your app is ready. Try switching font themes!
         </Text>
-        <Button size="3">Get Started</Button>
       </Flex>
     </Container>
   )
